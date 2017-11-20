6b44505649d4e4c0a9bac44c05c83a499b2c6021
Deshana Desai <desai.deshna@gmail.com>
2017-11-19 20:55:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
 	parser.add_argument('M', metavar='# weak_learners', help='number of weak learners', type=int)
 	parser.add_argument('trials', help='number of trials (each with different shuffling of the data); defaults to 1', type=int, default=1, nargs='?')
 	args = parser.parse_args()
-	
-	data = load_data("data/" + args.dataset)
 
-    accuracy, baseline = test(data, args.M, trials=args.trials)
-	
-    print "Accuracy:"
-    print accuracy
-    print "Baseline:"
+	X, y = load_svmlight_file(args.dataset)
+	data = zip(X, y)
+
+	accuracy, baseline = test(data, args.M, trials=args.trials)
+
+	print "Accuracy:"
+	print accuracy
+	print "Baseline:"
 	print baseline[-1]
