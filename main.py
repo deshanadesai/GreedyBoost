ab95a3cb572208bfd861ae108c66f629cc307d8a
Deshana Desai <desai.deshna@gmail.com>
2017-11-19 20:41:44 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+from random import seed
+
+if __name__ == "__main__":
+	seed(0)
+	parser.add_argument('dataset', help='dataset filename')
+	parser.add_argument('M', metavar='# weak_learners', help='number of weak learners', type=int)
+	parser.add_argument('trials', help='number of trials (each with different shuffling of the data); defaults to 1', type=int, default=1, nargs='?')
+	args = parser.parse_args()
+	
+	data = load_data("data/" + args.dataset)
+
+    accuracy, baseline = test(data, args.M, trials=args.trials)
+	
+    print "Accuracy:"
+    print accuracy
+    print "Baseline:"
+	print baseline[-1]
